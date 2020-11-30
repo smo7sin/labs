d0fc752192e42ba5c2fd1a402fb2b9a68a12fd23
Mohsin Syed <smohsin@live.com>
2020-11-30 14:24:16 -0500

    grades

--- fsx/grades.fsx
+++ fsx/grades.fsx
@@ -1,7 +1,14 @@
-open System
-open FSharp.Collections
+
+let nextMultiple x = 
+    let m = x % 5
+    if m > 0 then x - m + 5 else x
+
+let selectGrade (x,y) = if y - x < 3 then y else x
 
 let gradingStudents grades = 
-    grades 
-    |> Seq.map (float >> Math.Ceiling >> int)
+    grades |>
+    Seq.map ((fun x -> (x, nextMultiple x)) >> selectGrade) 
 
+let results = gradingStudents [73;67;38;33]
+
+results |> Seq.iter (printfn "%d")
